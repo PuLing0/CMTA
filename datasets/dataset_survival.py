fd909bac94b89777619f251fbeb1311ccbc29b6c
sjz <sijuzheng@mail.sdu.edu.cn>
2026-01-05 15:37:44 +0800

    Fix OOM patch subsampling in Generic_Split

--- datasets/dataset_survival.py
+++ datasets/dataset_survival.py
@@ -157,7 +157,7 @@ class Generic_WSI_Survival_Dataset(Dataset):
             mask = self.slide_data['slide_id'].isin(split.tolist())
             df_slice = self.slide_data[mask].reset_index(drop=True)
             split = Generic_Split(df_slice, metadata=self.metadata, modal=self.modal, signatures=self.signatures,
-                                  data_dir=self.data_dir, label_col=self.label_col, patient_dict=self.patient_dict, num_classes=self.num_classes)
+                                  data_dir=self.data_dir, label_col=self.label_col, patient_dict=self.patient_dict, num_classes=self.num_classes, OOM=getattr(self, 'OOM', 0))
         else:
             split = None
 
@@ -299,7 +299,7 @@ class Generic_MIL_Survival_Dataset(Generic_WSI_Survival_Dataset):
 
 
 class Generic_Split(Generic_MIL_Survival_Dataset):
-    def __init__(self, slide_data, metadata, modal, signatures=None, data_dir=None, label_col=None, patient_dict=None, num_classes=2):
+    def __init__(self, slide_data, metadata, modal, signatures=None, data_dir=None, label_col=None, patient_dict=None, num_classes=2, OOM=0):
         self.use_h5 = False
         self.slide_data = slide_data
         self.metadata = metadata
@@ -309,6 +309,8 @@ class Generic_Split(Generic_MIL_Survival_Dataset):
         self.label_col = label_col
         self.patient_dict = patient_dict
         self.slide_cls_ids = [[] for i in range(self.num_classes)]
+
+        self.OOM = OOM
 
         for i in range(self.num_classes):
             self.slide_cls_ids[i] = np.where(self.slide_data['label'] == i)[0]
